ec7952abdd732875ead5e4e8ffeebd17c2c8823d
Alex Barraza <diegobarrazapascual@hotmail.com>
2021-10-21 18:03:52 -0500

    add utils for saving and loading todos to localstorage

--- src/app/todo.service.ts
+++ src/app/todo.service.ts
@@ -1,25 +1,48 @@
 import {Injectable} from '@angular/core';
+import { NotifyService } from "./notify.service";
 import {ToDo} from "./ToDo";
+import {StorageService} from "@ng-web-apis/storage";
 
 @Injectable({
   providedIn: 'root'
 })
 export class TodoService {
 
-  constructor() {
-  }
+  constructor(
+    private notifyService: NotifyService,
+    private storageService: StorageService
+  ) {}
 
-  todos: ToDo[] = [];
+  todos: ToDo[] = this.loadToDos() ?? [];
 
   getTodos(): ToDo[] {
     return this.todos;
   }
 
+  getTodoById(id: number): ToDo | undefined {
+    let found = this.todos.find(t => t.id === id);
+    if (!found) return;
+    return found;
+  }
 
   addTodo(todo: ToDo) {
     if(todo === undefined) return;
     this.todos.push(todo);
     todo.id = this.todos.length === 0 ? 1 : this.todos.length;
-    console.log(this.todos)
+    this.notifyService.onTodoCreate(todo.id);
+    this.saveToDos();
+  }
+
+  //Loads ToDos from local storage
+  loadToDos() {
+    let todos = this.storageService.getItem("todos");
+    if (!todos) return;
+    return JSON.parse(todos)
+  }
+
+  //Save ToDos to local storage
+  saveToDos() {
+    let todos = JSON.stringify(this.todos);
+    this.storageService.setItem("todos", todos);
   }
 }
